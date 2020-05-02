584f89403ddafc9bc05ae3621717cfe57eaa58eb
Roman Vlasenko <bamp94@gmail.com>
2020-05-02 18:47:25 +0700

    add post endpoint, add validator, add post handler

--- controller/controller.go
+++ controller/controller.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4/middleware"
 
 	"cyberzilla_api_task/application"
@@ -30,6 +31,14 @@ func New(config config.Main, app application.Application) Controller {
 	}
 }
 
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
+
 // ServeHTTP http server
 func (c Controller) ServeHTTP(ctx context.Context, port int) {
 	c.initRoutes()
@@ -52,8 +61,25 @@ func (c Controller) initRoutes() {
 	// cors used for success answer on OPTION request from swagger
 	c.router.Use(middleware.CORS())
 
+	// init validator
+	c.router.Validator = &CustomValidator{validator: validator.New()}
+
 	c.router.GET("/swagger/*any", echoSwagger.WrapHandler)
 	c.router.GET("/healthcheck", c.healthcheck)
 
 	c.router.GET("/todos", c.getTodoList)
+	c.router.POST("/todos", c.createTodoTask)
+}
+
+func (c Controller) getAuthorizationToken(ctx echo.Context) string {
+	auth := ctx.Request().Header.Get("Authorization")
+	token := ctx.QueryParam("token")
+	switch {
+	case auth != "":
+		return auth
+	case token != "":
+		return token
+	default:
+		return ""
+	}
 }
